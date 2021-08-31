c24a8b4e5d5a4bea69b2298cabaec44b6e5ddf7a
mahbub <mahbub21alam2017@gmail.com>
2021-08-31 19:29:38 +0600

    Complete functions

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const colors = ["green, red, rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn= document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener('click', function(){
     const randomNumber =getRandomNumber();
-    console.log(randomNumber);
+    // console.log(randomNumber);
 
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
