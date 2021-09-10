8aa64403cbf9e5aa9aad142bcb1a87abbaeeb262
mahbub <mahbub21alam2017@gmail.com>
2021-09-10 22:52:23 +0600

    Complete counter  project

--- counter/app.js
+++ counter/app.js
@@ -1,15 +1,22 @@
 let count = 0;
 const value = document.querySelector('#value');
-const dec =document.querySelector('.decrease');
-const reset =document.querySelector('.reset');
-const inc =document.querySelector('.increase');
+// const dec =document.querySelector('.decrease');
+// const reset =document.querySelector('.reset');
+// const inc =document.querySelector('.increase');
+const btns = document.querySelectorAll('.btn')
 
-inc.addEventListener('click', function(e){
-    const styles = e.currentTarget.classList;
-    if(styles.contains('increase')){
-        count++;
-    }
-    value.textContent = count;
+btns.forEach(function (btn){
+    btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('increase')){
+            count++;
+        } else if(styles.contains('decrease')){
+            count--;
+        } else {
+            count = 0;
+        }
+        value.textContent = count;
+    })
 })
 
  
