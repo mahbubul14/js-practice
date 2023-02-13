fa104cdfa4a40a96653a740a1aa89f566af47d80
Mahbub <mahbub21alam2017@gmail.com>
2023-02-14 00:25:41 +0600

    Update basic things in js

---
+++ practice.js
@@ -0,0 +1,19 @@
+// var friendsName = false;
+// console.log(typeof friendsName);
+//variable, types, reserved keywords, js operators, function(if, switch), array, loop
+
+
+// var sum =2;
+// switch(sum) {
+//     case 1:
+//         console.log("hello world")
+//     break;
+//     case 10:
+//         console.log("We got ten")
+//     break;
+//     default:
+//         console.log("nothing match")
+// }
+
+var myDate = new Date();
+console.log(myDate.toTimeString());
