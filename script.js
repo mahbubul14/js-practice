22733e46f5f9c1f32520ca6bda448a555412e970
Mahbub <mahbub21alam2017@gmail.com>
2023-05-22 13:52:53 +0600

    Add event listener

---
+++ script.js
@@ -0,0 +1,19 @@
+const task_name =  document.getElementById("task-name");
+const submit = document.querySelector(".submit-btn")
+
+
+
+const ul = document.createElement("ul");
+const li =  document.createElement("li");
+submit.style.color = "red"
+
+submit.addEventListener('click', function() {
+    alert("hello");
+})
+
+
+
+// const el = document.getElementById("outside");
+// el.addEventListener(
+//   "click",
+//   () => {
