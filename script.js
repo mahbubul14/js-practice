9f84fb2b61a46675977496f276e91959afcbe730
Mahbub <mahbub21alam2017@gmail.com>
2023-05-23 12:16:15 +0600

    Add keypress and click both for the submit button

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const submit = document.querySelector(".submit-btn")
 const lists = document.getElementById("lists")
 submit.style.color = "red"
 
-submit.addEventListener('click',() => {
-    const task_text = task_input.value;
+const addTask = () => {
+        const  task_text = task_input.value.trim();
+    
     if (task_text != '') {
         const  listItem = document.createElement('li')
         listItem.innerHTML = `
@@ -12,9 +13,24 @@ submit.addEventListener('click',() => {
         <label>${task_text}</label>
         `;
         lists.appendChild(listItem);
+    
+        const removebtn =  document.createElement('button')
+        listItem.appendChild(removebtn);
+        removebtn.style.padding = "10px 10px 5px 10px";
 
         task_input.value ='';
+        lists.style.listStyle = `none`;
+    } else {
+        alert('Please enter a task')
     }
-    lists.style.listStyle = `none`;
-    console.log(lists)
+}
+
+task_input.addEventListener('keypress',(event) => {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+});
+
+submit.addEventListener('click',(event) => {
+        addTask();
 });
