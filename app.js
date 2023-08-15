04b40059c7d6ae46936e963b418e0ef92be3d9d3
Mahbub <mahbub21alam2017@gmail.com>
2023-08-15 19:56:49 +0600

    Add html for todo list

--- app.js
+++
@@ -1,9 +0,0 @@
-const names =  ['Harry', 'James', 'Potter', 'Mina', 'John']
-
-for (title of names) {
-      console.log(title);
-   if(title === 'Mina'){
-      console.log('James on the list')
-      break;
-   }
-}
